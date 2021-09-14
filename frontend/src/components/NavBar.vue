f7bcfe9bcae0225ac2de44e9a1d3efa55509fba5
gabrielfelipo <gabriel.felipo2010@gmail.com>
2021-09-14 00:27:24 -0300

    SideBar maked

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,15 +1,28 @@
 <template>
-<div>
-    
-</div>
+    <div class='navbar-container'>
+        <h1>{{Msg}}</h1>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    props: {
+        Msg: String
+    },
 }
 </script>
 
 <style scoped>
-
+/*#D0A85C rosa   #0BC6E3  azul   #111111  preto*/
+.navbar-container {
+    height: 100%;
+    width: 6vh;
+    background-color:  #D0A85C;
+    transition: 400ms;
+}
+.navbar-container:hover {
+    background-color: blueviolet;
+    width: 12vh;
+}
 </style>
