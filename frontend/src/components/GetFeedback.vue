bc4ce9bcb673240a781b2c5cd79248458b750edd
gabrielfelipo <gabriel.felipo2010@gmail.com>
2021-09-15 00:21:40 -0300

    Get component created

--- frontend/src/components/GetFeedback.vue
+++ frontend/src/components/GetFeedback.vue
@@ -1,15 +1,40 @@
 <template>
-<div>
-    
-</div>
+    <table class="table">
+        <thead>
+            <th>Name</th>
+            <th>Rating</th>
+            <th>Comment</th>
+        </thead>
+        <tbody>
+            <tr v-for="feedback in feedbacks" :key="feedback.id">
+                <td>{{feedback.name}}</td>
+                <td>{{feedback.rating}}</td>
+                <td>{{feedback.comment}}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
 export default {
-    name: 'GetFeedback'
+    name: 'GetFeedback',
+    data(){
+        return {
+            feedbacks: []
+        }
+    },
+        async created(){
+            var response = await fetch("http://127.0.0.1:8000/api/feedbackChamp/");
+            this.feedbacks = await response.json();
+        },
 }
 </script>
 
-<style scoped>
 
+
+<style scoped>
+.table {
+    width: 500px;
+    right: 0px;
+}
 </style>
