61fd178d9e105f54b81d9f543da55e65f8dce7d4
gabrielfelipo <gabriel.felipo2010@gmail.com>
2021-09-15 00:30:21 -0300

    Get component created

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class='navbar-container'>
         <h1>{{Msg}}</h1>
+        <img src="https://www.clipartmax.com/png/middle/32-321763_search-icon-free-png.png" alt="" class="search">
     </div>
 </template>
 
@@ -20,9 +21,15 @@ export default {
     width: 6vh;
     background-color:  #D0A85C;
     transition: 400ms;
+    position: absolute;
 }
+
 .navbar-container:hover {
     background-color: blueviolet;
     width: 12vh;
+}
+
+.search {
+    width: 6vh;
 }
 </style>
