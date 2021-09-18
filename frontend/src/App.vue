8752bed8aa4925f8200a8e58bb9097b5f00c4c20
gabrielfelipo <gabriel.felipo2010@gmail.com>
2021-09-17 21:26:20 -0300

    Routes recebidas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <NavBar Msg=""/>
-    <div class="Get">
-    <GetFeedback/>
-    </div>
+    <router-view/>
   </div>
 </template>
 
@@ -12,15 +9,12 @@
 
 <script>
 
-import NavBar from './components/NavBar.vue'
 //import SendFeedback from './components/SendFeedback.vue'
-import GetFeedback from './components/GetFeedback.vue'
+
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    GetFeedback,
   },
 }
 </script>
@@ -33,13 +27,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  height: 658px;
-  width: 1084px;
   text-align: center;
   color: #2c3e50;
-}
-.Get {
-  width: 50%;
-  left: 20%;
 }
 </style>
