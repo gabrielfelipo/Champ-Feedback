de2391a80b0655a0fa2a03699182b9b9ed2ae7e4
gabrielfelipo <gabriel.felipo2010@gmail.com>
2021-09-18 00:44:21 -0300

    Added navbar icons navegation

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,17 @@
 <template>
-    <div class='navbar-container'>
-        <h1>{{Msg}}</h1>
+    <div class="navbar-container">
+        <div class="move-navbar">
+            <a href="/home">Home</a>
+            <i class="menu-icon"><font-awesome-icon :icon="['fas','align-justify']"/></i>
+        </div>
+        <div class="send-navbar">
+            <a class="send-feedback" href="#">Send</a>
+            <i class="menu-icon fix"><font-awesome-icon :icon="['fas','edit']"/></i>
+        </div>
+        <div class="get-navbar">
+            <a class="send-feedback" href="#">View</a>
+            <i class="menu-icon fix"><font-awesome-icon :icon="['fas','list-alt']"/></i>
+        </div>
     </div>
 </template>
 
@@ -14,23 +25,64 @@ export default {
 </script>
 
 <style scoped>
+
 /*#D0A85C rosa   #0BC6E3  azul   #111111  preto*/
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap%27');
+
 .navbar-container {
-    height: 100%;
-    width: 15vh;
-    background-color:  rgba(37, 37, 37, 0.932);
-    transition: 400ms;
     position: absolute;
-    left: -7.5vh;
     z-index: 1;
+    transition: 0.3s;
+    left: -12vh;
+    padding: 2vh;
+    width: 18vh;
+    height: 100%;
+    text-decoration: none;
+    color: white;
+    background-color:  rgba(37, 37, 37, 0.829);
 }
 
-.navbar-container:hover {
-    background-color: #1b1b1bd8;
-    left: 0px;
+.navbar-container:hover{
+    left: -4vh;
+    background-color: #1b1b1bb9;
 }
 
-.search {
-    width: 6vh;
+a{
+    font-family:'Quicksand', sans-serif;
+    font-size: 2vh;
+    padding-left: 4vh;
+    text-align: center;
+    line-height: 1.7;
+    color: #ffffff;
+    margin: 0px;
+    transition: all 0.4s;
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
+    text-decoration: none;
+}
+
+a:hover{
+    opacity: 40%;
+}
+
+.menu-icon{
+    margin-left: 9%;
+}
+
+.move-navbar{
+    transform: translate(0vh, 30px);
+}
+
+.send-navbar{
+    transform: translate(0vh, 60px);
+}
+
+.get-navbar{
+    transform: translate(0vh, 90px);
+}
+
+.fix {
+    margin-left: 19%;
 }
 </style>
